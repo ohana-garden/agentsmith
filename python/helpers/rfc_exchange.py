664591d52d077d541ea9f7bab49fd0e8a8017da8
socean <socean@ohana.garden>
2026-01-10 02:22:36 -1000

    Fix rfc_exchange - read password directly, no RFC calls

--- python/helpers/rfc_exchange.py
+++ python/helpers/rfc_exchange.py
@@ -1,19 +1,13 @@
-from python.helpers import runtime, crypto, dotenv
+from python.helpers import dotenv
 
 async def get_root_password():
-    if runtime.is_dockerized():
-        pswd = _get_root_password()
-    else:
-        priv = crypto._generate_private_key()
-        pub = crypto._generate_public_key(priv)
-        enc = await runtime.call_development_function(_provide_root_password, pub)
-        pswd = crypto.decrypt_data(enc, priv)
-    return pswd
+    """Get root password directly from environment - no RFC needed"""
+    return _get_root_password()
     
 def _provide_root_password(public_key_pem: str):
-    pswd = _get_root_password()
-    enc = crypto.encrypt_data(pswd, public_key_pem)
-    return enc
+    """Legacy function - not used in containerized deployment"""
+    return _get_root_password()
 
 def _get_root_password():
     return dotenv.get_dotenv_value(dotenv.KEY_ROOT_PASSWORD) or ""
+
